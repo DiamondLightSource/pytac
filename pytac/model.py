a37cbc73e03d83fe3335a5ed087b71d48a2325f8
T-Nicholls <tobyn.nicholls@diamond.ac.uk>
2018-07-25 13:55:28 +0100

    Add description for methods and updated syntax and grammar.

--- pytac/model.py
+++ pytac/model.py
@@ -9,15 +9,13 @@ class Model(object):
     or a simulation.
 
     Attributes:
-        units (str): pytac.PHYS or pytac.ENG
+        units (str): pytac.PHYS or pytac.ENG.
     """
-
     def get_fields(self):
         """Get all the fields represented by this model.
 
         Returns:
-            iterable: all fields
-
+            iterable: all fields.
         """
         raise NotImplementedError()
 
@@ -25,12 +23,11 @@ class Model(object):
         """Get a value for a field.
 
         Args:
-            field (str): field of the requested value
+            field (str): field of the requested value.
             handle (str): pytac.RB or pytac.SP
 
         Returns:
-            float: value for specified field and handle
-
+            float: value for specified field and handle.
         """
         raise NotImplementedError()
 
@@ -40,17 +37,27 @@ class Model(object):
         This is always set to pytac.SP, never pytac.RB.
 
         Args:
-            field (str): field to set
-            value (str): value to set
-
+            field (str): field to set.
+            value (str): value to set.
         """
         raise NotImplementedError()
 
 
 class DeviceModel(object):
-    """Model containing control system devices."""
+    """Model containing control system devices.
 
+    **Attributes:**
+
+    Attributes:
+        units (str): pytac.ENG or pytac.PHYS, pytac.ENG by default.
+
+    .. Private Attributes:
+           _devices (dict): A dictionary of the devices for each key (field).
+    """
     def __init__(self):
+        """.. The constructor method for the class, called whenever a
+               'DeviceModel' object is constructed.
+        """
         self._devices = {}
         self.units = pytac.ENG
 
@@ -58,9 +65,8 @@ class DeviceModel(object):
         """Add device to this model.
 
         Args:
-            field (str): field this device represents
-            device (Device): device object
-
+            field (str): field this device represents.
+            device (Device): device object.
         """
         self._devices[field] = device
 
@@ -68,29 +74,49 @@ class DeviceModel(object):
         """Get device from the model.
 
         Args:
-            field (str): field of the requested device
-
+            field (str): field of the requested device.
+        Returns:
+            Device: The device of the specified field.
         """
         return self._devices[field]
 
     def get_fields(self):
+        """Get all the fields from the model.
+
+        Returns:
+            list: list of strings of all the fields of the model.
+        """
         return self._devices.keys()
 
     def get_pv_name(self, field, handle):
         """Get PV name for a field and handle.
 
         Args:
-            field (str): field of the requested PV
-            handle (str): pytac.RB or pytac.SP
+            field (str): field of the requested pv.
+            handle (str): pytac.RB or pytac.SP.
 
         Returns:
-            str: pv name for specified field and handle
-
+            str: pv name for specified field and handle.
         """
         return self._devices[field].get_pv_name(handle)
 
     def get_value(self, field, handle):
+        """Get the value of a readback or setpoint pv for a field from the model.
+
+        Args:
+            field (str): field of the requested value.
+            handle (str): pytac.RB or pytac.SP.
+
+        Returns:
+            float: The value of the pv.
+        """
         return self._devices[field].get_value(handle)
 
     def set_value(self, field, value):
+        """Set the value of a readback or setpoint pv for a field from the model.
+
+        Args:
+            field (str): field for the requested value.
+            value (float): The value to set on the pv.
+        """
         self._devices[field].set_value(value)
