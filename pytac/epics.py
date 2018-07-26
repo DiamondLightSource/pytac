e8b05e77a38c921805af968f4d4abf111f3f0ae3
T-Nicholls <tobyn.nicholls@diamond.ac.uk>
2018-07-26 17:55:28 +0100

    Remove trailing whitespace to fix error.

--- pytac/epics.py
+++ pytac/epics.py
@@ -1,4 +1,4 @@
-""" Module to implement an EpicsControlSystem object which is used to get 
+""" Module to implement an EpicsControlSystem object which is used to get
      real-time data from the machine.
 """
 
