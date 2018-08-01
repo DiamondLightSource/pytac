Fix minor spacing inconsistencies in the epics, units and utils modules.

pytac/utils.py
@@ -1,4 +1,4 @@
-""" Utility functions."""
+"""Utility functions."""
 
 import math
 import scipy.constants