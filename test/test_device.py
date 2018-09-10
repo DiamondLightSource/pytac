34cc2523af356a768622c32d54dea7a4b1fb8c7f
T-Nicholls <tobyn.nicholls@diamond.ac.uk>
2018-09-10 15:27:30 +0100

    Clean up epics device tests ready for new basic device tests.

--- test/test_device.py
+++ test/test_device.py
@@ -1,48 +1,54 @@
-import pytest
 import mock
+import pytac
+import pytest
 from pytac.epics import EpicsDevice, PvEnabler
 from pytac.exceptions import HandleException
 from constants import PREFIX, RB_PV, SP_PV
 
 
-def create_device(prefix=PREFIX, rb_pv=RB_PV, sp_pv=SP_PV, enabled=True):
+def create_epics_device(prefix=PREFIX, rb_pv=RB_PV, sp_pv=SP_PV, enabled=True):
     mock_cs = mock.MagicMock()
-    mock_cs.get.return_value = '1.0'
-    device = EpicsDevice(prefix, mock.MagicMock(), enabled=enabled, rb_pv=rb_pv,
+    mock_cs.get.return_value = 1.0
+    device = EpicsDevice(prefix, mock_cs, enabled=enabled, rb_pv=rb_pv,
                          sp_pv=sp_pv)
     return device
 
 
-def test_set_device_value():
-    device = create_device()
+def test_set_epics_device_value():
+    device = create_epics_device()
     device.set_value(40)
     device._cs.put.assert_called_with(SP_PV, 40)
 
 
-def test_device_invalid_sp_raise_exception():
-    device2 = create_device(PREFIX, RB_PV, None)
+def test_get_epics_device_value():
+    device = create_epics_device()
+    assert device.get_value(pytac.SP)==1.0
+
+
+def test_epics_device_invalid_sp_raises_exception():
+    device2 = create_epics_device(PREFIX, RB_PV, None)
     with pytest.raises(HandleException):
         device2.set_value(40)
 
 
-def test_get_device_value():
-    device = create_device()
+def test_get_epics_device_value_invalid_handle_raises_exception():
+    device = create_epics_device()
     with pytest.raises(HandleException):
         device.get_value('non_existent')
 
 
-def test_device_is_enabled_by_default():
-    device = create_device()
+def test_epics_device_is_enabled_by_default():
+    device = create_epics_device()
     assert device.is_enabled()
 
 
-def test_device_is_disabled_if_False_enabler():
-    device = create_device(enabled=False)
+def test_epics_device_is_disabled_if_False_enabler():
+    device = create_epics_device(enabled=False)
     assert not device.is_enabled()
 
 
-def test_device_is_enabled_returns_bool_value():
-    device = create_device(enabled=mock.MagicMock())
+def test_epics_device_is_enabled_returns_bool_value():
+    device = create_epics_device(enabled=mock.MagicMock())
     assert device.is_enabled() is True
 
 
