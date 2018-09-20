6f93751f18332989b42023f21d3dcc5bd7b6c4d3
T-Nicholls <tobyn.nicholls@diamond.ac.uk>
2018-09-20 14:45:01 +0100

    Update existing control systems to multiple/single syntax.

--- pytac/cothread_cs.py
+++ pytac/cothread_cs.py
@@ -3,7 +3,7 @@ from cothread.catools import caget, caput
 
 
 class CothreadControlSystem(ControlSystem):
-    """ The EPICS control system.
+    """The EPICS control system.
 
     It is used to communicate over channel access with the hardware
     in the ring.
@@ -13,8 +13,8 @@ class CothreadControlSystem(ControlSystem):
     def __init__(self):
         pass
 
-    def get(self, pv):
-        """ Get the value of a given PV.
+    def get_single(self, pv):
+        """Get the value of a given PV.
 
         Args:
             pv (string): The process variable given as a string. It can be a
@@ -25,11 +25,46 @@ class CothreadControlSystem(ControlSystem):
         """
         return caget(pv)
 
-    def put(self, pv, value):
-        """ Set the value for a given.
+    def get_multiple(self, pvs):
+        """Get the value for given PVs.
+
+        Args:
+            pvs (list): A list of process variables, given as a strings. They
+                         can be a readback or setpoint PVs.
+
+        Returns:
+            list: of floats, representing the current values of the PVs.
+
+        Raises:
+            ValueError: if the PVs are not passed in as a list.
+        """
+        if not isinstance(pvs, list):
+            raise ValueError('Please enter PVs as a list.')
+        return caget(pvs)
+
+    def set_single(self, pv, value):
+        """Set the value of a given PV.
 
         Args:
             pv (string): The PV to set the value of. It must be a setpoint PV.
             value (Number): The value to set the PV to.
         """
         caput(pv, value)
+
+    def set_multiple(self, pvs, values):
+        """Set the values for given PVs.
+
+        Args:
+            pvs (list): A list of PVs to set the values of. It must be a
+                         setpoint PV.
+            values (list): A list of the numbers to set no the PVs.
+
+        Raises:
+            ValueError: if the PVs or values are not passed in as a list, or if
+                         the lists of values and PVs are diffent lengths.
+        """
+        if not isinstance(pvs, list) and not isinstance(values, list):
+            raise ValueError('Please enter PVs and values as a list.')
+        elif len(pvs) != len(values):
+            raise ValueError('Please enter the same number of values as PVs.')
+        caput(pvs, values)
--- pytac/cs.py
+++ pytac/cs.py
@@ -6,22 +6,52 @@ class ControlSystem(object):
 
     **Methods:**
     """
-    def get(self, pv):
-        """ Get the value of the given PV.
+    def get_single(self, pv):
+        """Get the value of a given PV.
 
         Args:
-            pv (string): The PV to get the value of.
+            pv (string): The process variable given as a string. It can be a
+                         readback or a setpoint PV.
 
         Returns:
-            Number: The numeric value of the PV.
+            float: Represents the current value of the given PV.
         """
         raise NotImplementedError()
 
-    def put(self, pv, value):
-        """ Put the value of a given PV.
+    def get_multiple(self, pvs):
+        """Get the value for given PVs.
 
         Args:
-            pv (string): The PV to put the value for.
-            value (Number): The value to be set.
+            pvs (list): A list of process variables, given as a strings. They
+                         can be a readback or setpoint PVs.
+
+        Returns:
+            list: of floats, representing the current values of the PVs.
+
+        Raises:
+            ValueError: if the PVs are not passed in as a list.
+        """
+        raise NotImplementedError()
+
+    def set_single(self, pv, value):
+        """Set the value of a given PV.
+
+        Args:
+            pv (string): The PV to set the value of. It must be a setpoint PV.
+            value (Number): The value to set the PV to.
+        """
+        raise NotImplementedError()
+
+    def set_multiple(self, pvs, values):
+        """Set the values for given PVs.
+
+        Args:
+            pvs (list): A list of PVs to set the values of. It must be a
+                         setpoint PV.
+            values (list): A list of the numbers to set no the PVs.
+
+        Raises:
+            ValueError: if the PVs or values are not passed in as a list, or if
+                         the lists of values and PVs are diffent lengths.
         """
         raise NotImplementedError()
