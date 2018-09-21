d94bf07f626fcfc5e00273826a9a6403e1cec1e1
T-Nicholls <tobyn.nicholls@diamond.ac.uk>
2018-09-21 16:46:29 +0100

    Add timeout placeholders and complete set_multiple.

--- pytac/pyepics_cs.py
+++ pytac/pyepics_cs.py
@@ -24,7 +24,7 @@ class PyEpicsControlSystem(ControlSystem):
         Returns:
             float: Represents the current value of the given PV.
         """
-        return caget(pv)
+        return caget(pv, timeout='a_suitable_value')
 
     def get_multiple(self, pvs):
         """Get the value for given PVs.
@@ -50,7 +50,7 @@ class PyEpicsControlSystem(ControlSystem):
             pv (string): The PV to set the value of. It must be a setpoint PV.
             value (Number): The value to set the PV to.
         """
-        caput(pv, value)
+        caput(pv, value, wait=True, timeout='a_suitable_value')
 
     def set_multiple(self, pvs, values):
         """Set the values for given PVs.
@@ -68,4 +68,5 @@ class PyEpicsControlSystem(ControlSystem):
             raise ValueError('Please enter PVs and values as a list.')
         elif len(pvs) != len(values):
             raise ValueError('Please enter the same number of values as PVs.')
-        pass
+        for pv, value in zip(pvs, values):
+            caput(pv, value, wait=False, timeout='a_suitable_value')
