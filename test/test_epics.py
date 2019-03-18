ba96d0e4eec11e66c573b7f483a391729b3d320e
T-Nicholls <tobyn.nicholls@diamond.ac.uk>
2019-03-18 14:55:45 +0000

    Fix travis failure.

--- test/test_epics.py
+++ test/test_epics.py
@@ -51,11 +51,11 @@ def test_get_value_uses_cs_if_data_source_live(simple_epics_element):
     simple_epics_element.get_value('x', handle=pytac.SP,
                                    data_source=pytac.LIVE)
     simple_epics_element.get_device('x')._cs.get_single.assert_called_with(SP_PV,
-                                                                       True)
+                                                                           True)
     simple_epics_element.get_value('x', handle=pytac.RB,
                                    data_source=pytac.LIVE)
     simple_epics_element.get_device('x')._cs.get_single.assert_called_with(RB_PV,
-                                                                       True)
+                                                                           True)
 
 
 def test_get_value_raises_HandleExceptions(simple_epics_element):
