d7c7ef00b2a892855dfb6a6bbaaf90f8069b85de
JoshuaAppleby <JoshuaAppleby@users.noreply.github.com>
2023-04-12 13:56:33 +0000

    deploy: 05435f2735f116fc997d414ad88402678a032af8

--- cothread_cs-wait/_static/documentation_options.js
+++ cothread_cs-wait/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.1.dev65+g525818e',
+    VERSION: '0.5.1.dev66+g05435f2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
