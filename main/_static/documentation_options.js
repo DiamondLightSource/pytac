d62012f448a16c1b74e9f3effe35799b2f59e909
MJGaughran <MJGaughran@users.noreply.github.com>
2024-07-12 15:24:25 +0000

    deploy: 6f3639097953c530ce9f775cf62d8990daf1adce

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.1.dev68+g7411792',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.6.1.dev16+g6f36390',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
@@ -8,7 +7,7 @@ var DOCUMENTATION_OPTIONS = {
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: true,
+    NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
 };
