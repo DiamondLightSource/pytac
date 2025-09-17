bbf846ef3acf5f299d90e57fea0a3b378fcd6b9c
ptsOSL <ptsOSL@users.noreply.github.com>
2025-09-17 10:51:04 +0000

    deploy: 16fb10e315175c93f7af341a1691f47a7c13ded1

--- diamond2-conversion/_static/documentation_options.js
+++ diamond2-conversion/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.6.3.dev16+ga360d76a9',
+    VERSION: '0.6.3.dev17+g16fb10e31',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
