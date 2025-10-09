fb50184ab0e5ea6e598273e849dfc7ca56fbf1af
ptsOSL <ptsOSL@users.noreply.github.com>
2025-10-09 13:29:15 +0000

    deploy: 0758375a93ca76ee56258e61c1fd1f9d19e904cf

--- asyncio-conversion/_static/documentation_options.js
+++ asyncio-conversion/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.0.1.dev10+geb0582fc0',
+    VERSION: '1.0.1.dev11+g0758375a9',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
